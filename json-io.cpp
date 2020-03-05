298f2aa735da0af3e9cecf888f383805aff1e080
Noname Kamisawa <noname@kamisawa.net>
2020-03-06 00:09:04 +0900

    goods.jsonが読み込まれない不具合修正

--- json-io.cpp
+++ json-io.cpp
@@ -1,8 +1,8 @@
 #include "json-io.h"
 
-JsonDocument& JsonIO::OpenJson(const char* file_name)
+void JsonIO::OpenJson(const char* file_name, JsonDocument& json_document)
 {
-    _json_document.clear();
+    json_document.clear();
 
     File file = SD.open(file_name, FILE_READ);
 
@@ -16,25 +16,24 @@ JsonDocument& JsonIO::OpenJson(const char* file_name)
 
     file.close();
 
-    DeserializationError error = deserializeJson(_json_document, json_text);
+    DeserializationError error = deserializeJson(json_document, json_text);
 
     if (error != DeserializationError::Code::Ok)
     {
         _serial->Print("JSON deserialize failed: ");
         _serial->Println(file_name);
-        return _json_document;
     }
-
-    return _json_document;
 }
 
 void JsonIO::Read()
 {
-    // todo: goods.json, sales.json に分離
-    auto json_goods = OpenJson(goods_file);
-    auto json_sales = OpenJson(sales_file);
-
+    // ファイルごとにデシリアライズしてJsonArrayへ展開
+    StaticJsonDocument<goods_size> json_goods;
+    OpenJson(goods_file, json_goods);
     JsonArray goods = json_goods[goods_key].as<JsonArray>();
+
+    StaticJsonDocument<sales_size> json_sales;
+    OpenJson(sales_file, json_sales);
     JsonArray sales = json_sales[goods_key].as<JsonArray>();
     JsonArray amounts = json_sales[amounts_key].as<JsonArray>();
 
@@ -44,16 +43,17 @@ void JsonIO::Read()
 
 void JsonIO::Write()
 {
-    _json_document.clear();
+    // Salesのみシリアライズ
+    StaticJsonDocument<sales_size> json_document;
 
-    JsonArray sales = _json_document.createNestedArray(goods_key);
-    JsonArray amounts = _json_document.createNestedArray(amounts_key);
+    JsonArray sales = json_document.createNestedArray(goods_key);
+    JsonArray amounts = json_document.createNestedArray(amounts_key);
 
     _goods_list->Serialize(sales);
     _amount_state->Serialize(amounts);
 
     String json_text;
-    serializeJsonPretty(_json_document, json_text);
+    serializeJsonPretty(json_document, json_text);
 
     File file = SD.open(sales_file, FILE_WRITE);
     file.println(json_text);
--- json-io.h
+++ json-io.h
@@ -12,19 +12,20 @@
 
 class JsonIO
 {
+    static constexpr const size_t goods_size = 2048;
+    static constexpr const size_t sales_size = 1024;
+
     static constexpr const char* goods_key = "goods";
     static constexpr const char* amounts_key = "amounts";
 
-    static constexpr const char* goods_file = "goods.json";
-    static constexpr const char* sales_file = "sales.json";
+    static constexpr const char* goods_file = "/goods.json";
+    static constexpr const char* sales_file = "/sales.json";
 
     ISerial* const _serial;
     GoodsList* const _goods_list;
     AmountState* const _amount_state;
 
-    StaticJsonDocument<json_size> _json_document = StaticJsonDocument<json_size>();
-
-    JsonDocument& OpenJson(const char* file_name);
+    void OpenJson(const char* file_name, JsonDocument& json_document);
 
 public:
     JsonIO(ISerial* const serial, GoodsList* const goods_list, AmountState* const amount_state) : _serial(serial), _goods_list(goods_list), _amount_state(amount_state)
